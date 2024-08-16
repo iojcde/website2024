c10ad4fea5eeca1398192fdfa5082119e3e028d8
Jeeho Ahn <io@jcde.xyz>
2024-08-16 09:45:45 +0900

    yeah...

--- app/blocks/Code/Code.client.tsx
+++ app/blocks/Code/Code.client.tsx
@@ -1,20 +1,22 @@
-'use client'
-import { Highlight, themes } from 'prism-react-renderer'
-import React from 'react'
+"use client";
+import { Highlight, themes } from "prism-react-renderer";
+import React from "react";
 
 type Props = {
-  code: string
-  language?: string
-}
+  code: string;
+  language?: string;
+};
 
 export const Code: React.FC<Props> = ({ code, language }) => {
   return (
-    <Highlight code={code} language={language} theme={themes.vsDark}>
+    <Highlight code={code} language={language as string} theme={themes.vsDark}>
       {({ getLineProps, getTokenProps, tokens }) => (
         <pre className="bg-black p-4 border text-xs border-border rounded overflow-x-auto">
           {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ className: 'table-row', line })}>
-              <span className="table-cell select-none text-right text-white/25">{i + 1}</span>
+            <div key={i} {...getLineProps({ className: "table-row", line })}>
+              <span className="table-cell select-none text-right text-white/25">
+                {i + 1}
+              </span>
               <span className="table-cell pl-4">
                 {line.map((token, key) => (
                   <span key={key} {...getTokenProps({ token })} />
@@ -25,5 +27,5 @@ export const Code: React.FC<Props> = ({ code, language }) => {
         </pre>
       )}
     </Highlight>
-  )
-}
+  );
+};
