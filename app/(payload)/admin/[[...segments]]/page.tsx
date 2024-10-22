021779f1e71ac5a92ee05982e27cda80b9b921f0
Jeeho Ahn <io@jcde.xyz>
2024-10-22 13:30:12 +0900

    hi

--- app/(payload)/admin/[[...segments]]/page.tsx
+++ app/(payload)/admin/[[...segments]]/page.tsx
@@ -1,29 +1,26 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
-import type { Metadata } from 'next'
-
-import config from '@payload-config'
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
-import { RootPage, generatePageMetadata } from '@payloadcms/next/views'
+import type { Metadata } from "next";
+
+import config from "@payload-config";
+import { generatePageMetadata, RootPage } from "@payloadcms/next/views";
 
 type Args = {
   params: {
-    segments: string[]
-  }
+    segments: string[];
+  };
   searchParams: {
-    [key: string]: string | string[]
-  }
-}
+    [key: string]: string | string[];
+  };
+};
 
-export const generateMetadata = async (props: Args): Promise<Metadata> => {
-  const searchParams = await props.searchParams;
-  const params = await props.params;
-  return generatePageMetadata({ config, params, searchParams });
-}
+export const generateMetadata = ({
+  params,
+  searchParams,
+}: Args): Promise<Metadata> =>
+  generatePageMetadata({ config, params, searchParams });
 
-const Page = async (props: Args) => {
-  const searchParams = await props.searchParams;
-  const params = await props.params;
-  return RootPage({ config, params, searchParams });
-}
+const Page = ({ params, searchParams }: Args) =>
+  RootPage({ config, params, searchParams });
 
-export default Page
+export default Page;
